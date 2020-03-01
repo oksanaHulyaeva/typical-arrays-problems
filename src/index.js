96f57c63d1d5c7eebd989945409388d7c1305d0b
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2020-03-01 23:34:14 +0300

    feat: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  else {
+    let minEl = array[0];
+    for(let i = 0; i< array.length; i++){
+      if(minEl > array[i]) minEl = array[i];
+    }
+    return minEl;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  else {
+    let maxEl = array[0];
+    for(let i = 0; i< array.length; i++){
+      if(maxEl < array[i]) maxEl = array[i];
+    }
+    return maxEl;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+  else {
+    let average = 0;
+    for(let i = 0; i<array.length; i++){
+      average += array[i];
+    }
+    return (average / array.length);
+  }
 }
